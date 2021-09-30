3f5cfd7e5172f171df84327229b94500aafcd0b0
arijeetray35 <73897397+arijeetray35@users.noreply.github.com>
2021-09-30 13:40:20 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "EXODUS";
+const accessCode2 = "EXCALLIBUR";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("S D O U X E", 100,50)
+    fill("lightblue")
+    text("Hint: Chapter in Bible", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C X E A L L I R U B", 700,150)
+    fill("lightblue")
+    text("Hint: Satelite's name !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+}
